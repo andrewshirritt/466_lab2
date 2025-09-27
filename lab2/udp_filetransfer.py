7dd233883ce0c813ff81eba8919e713cfcc7d9fa
andrewshirritt <s30461@rmc-cmr.ca>
2025-09-27 12:11:10 -0400

    Add files via upload

--- lab2/udp_filetransfer.py
+++ lab2/udp_filetransfer.py
@@ -8,7 +8,7 @@ from comm_interface import CommInterface
 random.seed(1)
 DROP_PROBABILITY = 0.1
 DUPLICATE_PROBABILITY = 0.1
-LAG_PROBABILITY = 0.1
+LAG_PROBABILITY = 0.5
 
 CHUNK_SIZE = 1024
 
@@ -20,6 +20,8 @@ class UDPFileTransfer(CommInterface):
 
     def __init__(self):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.recvsnumber = 0
+        self.sendsnumber = 0
 
     def initialize_as_server(self, host, port):
         self.socket.bind((host, port))
@@ -38,69 +40,187 @@ class UDPFileTransfer(CommInterface):
         self.socket.sendto(message, dest_addr)
 
     def send_message(self, data, filename="", addr=None):
-        message = data + ":" + filename
-        self.socket.sendto(message.encode(), addr)
+        message = f"{data}:{filename}:{self.sendsnumber}"
+        encoded_msg = message.encode()
+        print(f"Sending {self.sendsnumber}")
+        self._send(encoded_msg, addr)
+        if data != 'QUT':
+            while True:
+                try:
+                    ack, _ = self.socket.recvfrom(self.CHUNK_SIZE)
+                    if ack.startswith(b'ack:'):
+                        ack_num = int(ack.decode().split(":")[1])
+                        if ack_num == self.sendsnumber:
+                            print(f"Received ack for {ack_num}")
+                            self.sendsnumber += 1
+                            break
+                except:
+                    print(f"Timeout or error, resending {self.sendsnumber}")
+                    self._send(encoded_msg, addr)
+        else:
+            for i in range(10):
+                self._send(encoded_msg, addr)
+
+
+
+        # message = data + ":" + filename + ":" + str(self.sendsnumber)
+        # print(self.sendsnumber)
+        # self._send(message.encode(), addr)
+        #
+        #
+        # while True:
+        #     try:
+        #         message, addr = self.socket.recvfrom(self.CHUNK_SIZE)
+        #         if message == b'ack':
+        #             self._send(b'ack', addr)
+        #             self.sendsnumber += 1
+        #             break
+        #
+        #     except:
+        #         self._send(message.encode(), addr)
+        #         print(f"sending again {self.sendsnumber}")
+
+
+
+
 
 
 
     def send_file(self, filepath, addr):
-        snumber = 1
-        with open(filepath, "rb") as f:
-            while True:
-                chunk = f.read(self.CHUNK_SIZE-50)
-
-                if not chunk:
-                    break
-                chunk = f'{snumber}'.encode() + '|||'.encode() + chunk
-                #self._send(chunk, addr)
-                self._send(chunk, addr)
-                #Replace this with
-                #Ack#server Ack#client Ack#server#++
-                #if Ack# not equal when returned:
-                while True:
-                    try :
-                        self.socket.recvfrom(CHUNK_SIZE)
-                        #if client_ack == snumber:
-                        break
-                    except:
-
-                        self._send(chunk, addr)
-                        print("sending again")
-
-                snumber += 1
-
-        chunk = f'{snumber}|||end'.encode()
-        self.socket.sendto(chunk, addr)
-
+        pass
+        # snumber = 1
+        # with open(filepath, "rb") as f:
+        #     while True:
+        #         chunk = f.read(self.CHUNK_SIZE-50)
+        #
+        #         if not chunk:
+        #             break
+        #         chunk = f'{snumber}'.encode() + '|||'.encode() + chunk
+        #         #self._send(chunk, addr)
+        #         self._send(chunk, addr)
+        #         #Replace this with
+        #         #Ack#server Ack#client Ack#server#++
+        #         #if Ack# not equal when returned:
+        #         while True:
+        #             try :
+        #                 self.socket.recvfrom(CHUNK_SIZE)
+        #                 #if client_ack == snumber:
+        #                 break
+        #             except:
+        #
+        #                 self._send(chunk, addr)
+        #                 print("sending again")
+        #
+        #         snumber += 1
+        #
+        # chunk = f'{snumber}|||end'.encode()
+        # self._send(chunk, addr)
+        # while True:
+        #     try:
+        #         self.socket.recvfrom(CHUNK_SIZE)
+        #         # if client_ack == snumber:
+        #         break
+        #     except:
+        #
+        #         self._send(chunk, addr)
+        #         print("sending again")
 
     def receive_message(self):
-        message, addr = self.socket.recvfrom(self.CHUNK_SIZE)
+        while True:
+            try:
+                message, addr = self.socket.recvfrom(self.CHUNK_SIZE)
+                if message.startswith(b'ack:'):
+                    continue  # ignore ack packets
 
-        com, filename = message.decode().split(":")
+                com, filename, snumber = message.decode().split(":")
+                snumber = int(snumber)
+
+                if snumber == self.recvsnumber:
+                    print(f"Received new message {snumber}")
+                    ack_msg = f"ack:{snumber}".encode()
+                    self._send(ack_msg, addr)
+                    self.recvsnumber += 1
+                    return com, filename, addr
+                else:
+                    # Duplicate or out-of-order — re-ack
+                    print(f"Duplicate or old message {snumber}, expected {self.recvsnumber}")
+                    ack_msg = f"ack:{snumber}".encode()
+                    self._send(ack_msg, addr)
+            except:
+                continue
+
+
+
+
+        self._send(b'ack', addr)
+        self.recvsnumber += 1
+        print(self.recvsnumber)
         return com, filename, addr
+        # while True:
+        #     try:
+        #         message, addr = self.socket.recvfrom(self.CHUNK_SIZE)
+        #         if message == b'ack':
+        #             continue
+        #         com, filename, snumber = message.decode().split(":")
+        #         #print(snumber)
+        #         #print(self.recvsnumber)
+        #
+        #         snumber = int(snumber)
+        #         if snumber == self.recvsnumber:
+        #             self._send(b'ack', addr)
+        #             continue
+        #         break
+        #     except:
+        #         continue
+        #
+        # self._send(b'ack', addr)
+        # while True:
+        #     try:
+        #         message, addr = self.socket.recvfrom(self.CHUNK_SIZE)
+        #         print('hi')
+        #         if message != b'ack':
+        #             self._send(b'ack', addr)
+        #         else:
+        #             break
+        #     except:
+        #         continue
+        #
+        # self.recvsnumber +=1
+        # print(self.recvsnumber)
+        #
+        #
+        # return com, filename, addr
 
     def receive_file(self, filepath):
-        snumber = 0
-        with open(filepath, "wb") as f:
-            while True:
-                previous = snumber
-                while True:
-
-                    try:
-                        chunk, addr = self.socket.recvfrom(self.CHUNK_SIZE)
-                        break
-                    except:
-                        pass
-
-
-                snumber, message = chunk.split(b'|||')
-                if snumber == previous:
-                    continue
-
-
-
-
-                if message == b'end':
-                    break
-                self.socket.sendto(snumber, addr)
-                f.write(message)
+        pass
+        # snumber = 0
+        # with open(filepath, "wb") as f:
+        #     while True:
+        #         previous = snumber
+        #         while True:
+        #
+        #             try:
+        #                 chunk, addr = self.socket.recvfrom(self.CHUNK_SIZE)
+        #                 if chunk == b'ack':
+        #                     continue
+        #                 break
+        #             except:
+        #                 pass
+        #
+        #         try:
+        #             snumber, message = chunk.split(b'|||')
+        #         except:
+        #             self._send(b'ack', addr)
+        #             continue
+        #
+        #         if snumber == previous:
+        #             continue
+        #
+        #
+        #
+        #
+        #         if message == b'end':
+        #             self.socket.sendto(b'ack', addr)
+        #             break
+        #         self.socket.sendto(snumber, addr)
+        #         f.write(message)
